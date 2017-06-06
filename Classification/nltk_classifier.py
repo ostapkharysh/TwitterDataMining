a1551b03201c86b7d4e886fafd4c44c00a1e881b
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-06-07 01:28:39 +0300

    nltk Classification

--- Classification/nltk_classifier.py
+++ Classification/nltk_classifier.py
@@ -1,8 +1,12 @@
 import nltk
 import random
+from ukrainian_stemmer import *
 from nltk.tokenize import TweetTokenizer
 import re
 from stop_words import get_stop_words
+
+
+stemmer = UkrainianStemmer()
 
 all_stopwords = []
 all_stopwords.extend(get_stop_words("ukrainian"))
@@ -40,13 +44,25 @@ def read_json(filename):
 schur = read_json("dataMichaelSchur.json")
 poroshenko = read_json("dataPetroPoroshenko.json")
 
-common = []
-for i in schur:
-    common.extend(i.split())
-for i in poroshenko:
-    common.extend(i.split())
-
 categories = {"poroshenko": poroshenko, "schur": schur}
+
+########################## STEMMING ####################################################
+class_words = {}
+
+classes = list(set([a for a in categories.keys()]))
+for c in classes:
+    class_words[c] = []
+
+for data in categories.keys():
+    for i in categories[data]:
+        for word in nltk.word_tokenize(i):
+            stemmed_word = stemmer.stem_word(word)
+            class_words[data].extend([stemmed_word])
+
+common = []
+for i in class_words.keys():
+    common.extend(class_words[i])
+########################################################################################
 
 documents = [(j.split(), i) for i in categories.keys() for j in categories[i]]
 for i in categories.keys():
@@ -54,19 +70,15 @@ for i in categories.keys():
         t = (j.split(), i)
         documents.append(t)
 random.shuffle(documents)
-
-all_words = nltk.FreqDist(w.lower() for w in common)
-word_features = list(all_words)[:2000]
+all_words = nltk.FreqDist(w for w in common)
+word_features = all_words.most_common(2000)
 
 def document_features(document):
     document_words = set(document)
     features = {}
     for word in word_features:
-        features['contains({})'.format(word)] = (word in document_words)
+        features['contains({})'.format(word[0])] = (word[0] in document_words)
     return features
-
-# print(document_features(poroshenko[1485]))
-# print(document_features(schur[1485]))
 
 featuresets = [(document_features(d), c) for (d,c) in documents]
 n = len(featuresets)
@@ -75,9 +87,9 @@ classifier = nltk.NaiveBayesClassifier.train(train_set)
 print(nltk.classify.accuracy(classifier, test_set))
 classifier.show_most_informative_features(5)
 
-test_sentence = "Слава Україні!"
+# test_sentence = "Слава Україні!"
 test_sentence2 = "Україна повертається до європейської родини"
-test_sent_features = {word.lower(): (word in nltk.word_tokenize(test_sentence.lower())) for word in all_words}
+# test_sent_features = {word.lower(): (word in nltk.word_tokenize(test_sentence.lower())) for word in all_words}
 test_sent_features2 = {word.lower(): (word in nltk.word_tokenize(test_sentence2.lower())) for word in all_words}
-print(classifier.classify(test_sent_features))
+# print(classifier.classify(test_sent_features))
 print(classifier.classify(test_sent_features2))
