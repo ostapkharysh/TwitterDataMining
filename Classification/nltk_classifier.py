d8d7e23ece56cb9f5a8d849ba8e1ad22f96fd045
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-06-12 21:41:25 +0300

    NLTK Classification

--- Classification/nltk_classifier.py
+++ Classification/nltk_classifier.py
@@ -5,9 +5,6 @@ from nltk.tokenize import TweetTokenizer
 import re
 from stop_words import get_stop_words
 
-
-stemmer = UkrainianStemmer()
-
 all_stopwords = []
 all_stopwords.extend(get_stop_words("ukrainian"))
 all_stopwords.extend(get_stop_words("english"))
@@ -43,34 +40,50 @@ def read_json(filename):
 
 schur = read_json("dataMichaelSchur.json")
 poroshenko = read_json("dataPetroPoroshenko.json")
+avakov = read_json("dataAvakovEXT.json")
+tkach = read_json("dataMychailoTkachEXT.json")
+liashko = read_json("dataOVLiashkoEXT.json")
+saakashvili = read_json("dataSaakashviliEXT.json")
+sadovyi = read_json("dataSadovyiEXT.json")
+sheremeta = read_json("dataSheremetaEXT.json")
+shevchenko = read_json("dataShevchenkoEXT.json")
+skrypin = read_json("dataSkrypinEXT.json")
+suprun = read_json("dataSuprunEXT.json")
+vakarchuk = read_json("dataVakarchukEXT.json")
 
-categories = {"poroshenko": poroshenko, "schur": schur}
+categories = {"Schur": schur, "Avakov": avakov, "Poroshenko": poroshenko, "Tkach": tkach, "Liashko": liashko,
+              "Saakashvili": saakashvili, "Sadovyi": sadovyi, "Sheremeta": sheremeta, "Shevchenko": shevchenko,
+              "Skrypin": skrypin, "Suprun": suprun, "Vakarchuk": vakarchuk}
 
 ########################## STEMMING ####################################################
-class_words = {}
+stemmer = UkrainianStemmer()
 
-classes = list(set([a for a in categories.keys()]))
-for c in classes:
-    class_words[c] = []
-
-for data in categories.keys():
-    for i in categories[data]:
-        for word in nltk.word_tokenize(i):
-            stemmed_word = stemmer.stem_word(word)
-            class_words[data].extend([stemmed_word])
-
-common = []
-for i in class_words.keys():
-    common.extend(class_words[i])
+def stemming():
+    class_words = {}
+    
+    classes = list(set([a for a in categories.keys()]))
+    for c in classes:
+        class_words[c] = []
+    
+    for data in categories.keys():
+        for i in categories[data]:
+            for word in nltk.word_tokenize(i):
+                stemmed_word = stemmer.stem_word(word)
+                class_words[data].extend([stemmed_word])
+    common = []
+    for i in class_words.keys():
+        common.extend(class_words[i])
+    return common
 ########################################################################################
 
+common_lst = stemming()
 documents = [(j.split(), i) for i in categories.keys() for j in categories[i]]
 for i in categories.keys():
     for j in categories[i]:
         t = (j.split(), i)
         documents.append(t)
 random.shuffle(documents)
-all_words = nltk.FreqDist(w for w in common)
+all_words = nltk.FreqDist(w for w in common_lst)
 word_features = all_words.most_common(2000)
 
 def document_features(document):
@@ -80,16 +93,18 @@ def document_features(document):
         features['contains({})'.format(word[0])] = (word[0] in document_words)
     return features
 
-featuresets = [(document_features(d), c) for (d,c) in documents]
+featuresets = [(document_features(d), c) for (d, c) in documents]
 n = len(featuresets)
-train_set, test_set = featuresets[n // 2:], featuresets[:n // 2]
+
+train_set, test_set = featuresets[:n // 2], featuresets[n // 2:]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
+
 print(nltk.classify.accuracy(classifier, test_set))
 classifier.show_most_informative_features(5)
 
-# test_sentence = "Слава Україні!"
+test_sentence = "Затримано 24 фігуранта, веземо до Києва"
 test_sentence2 = "Україна повертається до європейської родини"
-# test_sent_features = {word.lower(): (word in nltk.word_tokenize(test_sentence.lower())) for word in all_words}
+test_sent_features = {word.lower(): (word in nltk.word_tokenize(test_sentence.lower())) for word in all_words}
 test_sent_features2 = {word.lower(): (word in nltk.word_tokenize(test_sentence2.lower())) for word in all_words}
-# print(classifier.classify(test_sent_features))
+print(classifier.classify(test_sent_features))
 print(classifier.classify(test_sent_features2))
